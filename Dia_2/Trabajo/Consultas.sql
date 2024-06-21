4644bfd43eea7ae323f741a0fc95f2d30af39b8c
JairGarcia <156432366+JairBGarcia@users.noreply.github.com>
2024-06-21 07:55:54 -0500

    feat :sparkles: Correccion Consultas

--- Dia_2/Trabajo/Consultas.sql
+++ Dia_2/Trabajo/Consultas.sql
@@ -4,218 +4,199 @@ use TrabajoDia2;
 -- #############################
 -- Consultas sobre una tabla
 
+use TrabajoDia2;
+
 -- 1.Lista el primer apellido de todos los empleados.
-select apellido1
+    delimiter //
+    create procedure eje1()
+    begin
+    select apellido1
 from empleado;
+    end //
+    delimiter ;
+    call eje1();
 
 -- 2.Lista el primer apellido de los empleados eliminando los apellidos que estén repetidos.
+    delimiter //
+    create procedure eje2()
+    begin
 select distinct apellido1
 from empleado;
+    end //
+    delimiter ;
+call eje2();
 
 -- 3.Lista todas las columnas de la tabla empleado.
+    delimiter //
+    create procedure eje3()
+    begin
 select *
 from empleado;
+    end //
+    delimiter ;
+    call eje3();
 
 -- 4.Lista el nombre y los apellidos de todos los empleados.
+    delimiter //
+    create procedure eje4()
+    begin
 select nombre, apellido1 as apellido, apellido2 as segundo_apellido
 from empleado;
-
+    end //
+    delimiter ;
+    call eje4();
+    
 -- 5.Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado
+    delimiter //
+    create procedure eje5()
+    begin
 select id_departamento 
 from empleado;
+    end //
+    delimiter ;
+call eje5();
 
 -- 6.Lista el identificador de los departamentos de los empleados que aparecen
 -- en la tabla empleado, eliminando los identificadores que aparecen repetidos.
-select distinct id_departamento
+    delimiter //
+    create procedure eje6()
+    begin
+    select distinct id_departamento
 from empleado;
+    end //
+    delimiter ;
+    call eje6();
 
 -- 7.Lista el nombre y apellidos de los empleados en una única columna.
-select CONCAT(nombre, ' ', apellido1, ' ', apellido2) as nombre_completo 
+    delimiter //
+    create procedure eje7()
+    begin
+    select CONCAT(nombre, ' ', apellido1, ' ', apellido2) as nombre_completo 
 from empleado;
+    end //
+    delimiter ;
+call eje7();
 
 -- 8.Lista el nombre y apellidos de los empleados en una única columna,
 -- convirtiendo todos los caracteres en mayúscula.
-select ucase(concat(nombre, ' ', apellido1, '' , apellido2)) as nombre_completo
+    delimiter //
+    create procedure eje8()
+    begin
+    select ucase(concat(nombre, ' ', apellido1, '' , apellido2)) as nombre_completo
 from empleado;
+    end //
+    delimiter ;
+call eje8();
 
 -- 9.Lista el nombre y apellidos de los empleados en una única columna,
 -- convirtiendo todos los caracteres en minúscula.
-select lower(concat(nombre, ' ', apellido1, ' ' , apellido2)) as nombre_complete
+    delimiter //
+    create procedure eje9()
+    begin
+    select lower(concat(nombre, ' ', apellido1, ' ' , apellido2)) as nombre_complete
 from empleado;
+    end //
+    delimiter ;
+call eje9();
 
--- 10. Lista el identificador de los empleados junto al nif, pero el nif deberá
--- aparecer en dos columnas, una mostrará únicamente los dígitos del nif y la
--- otra la letra.
+-- 10.Lista el identificador de los empleados junto al nif, pero el nif deberá
+	-- aparecer en dos columnas, una mostrará únicamente los dígitos del nif y la
+	-- otra la letra.
+    delimiter //
+    create procedure eje10()
+    begin
+    	select id, 
+		left(nif, LENGTH(nif) - 1) as nif_digitos, 
+		right(nif, 1) as nif_letra 
+		from empleado;
+    end //
+    delimiter ;
+call eje10();
 
-select convert(id, char) as id,
-       substring(nif, 1, 8) as nif_digitos,
-       substring(nif, 9, 1) as nif_letra
-from empleado;
+	-- 11.Lista el nombre de cada departamento y el valor del presupuesto actual del
+	-- que dispone. Para calcular este dato tendrá que restar al valor del
+	-- presupuesto inicial (columna presupuesto) los gastos que se han generado
+	-- (columna gastos). Tenga en cuenta que en algunos casos pueden existir
+	-- valores negativos. Utilice un alias apropiado para la nueva columna columna
+	-- que está calculando.
+delimiter //
 
--- 11. Lista el nombre de cada departamento y el valor del presupuesto actual del
--- que dispone. Para calcular este dato tendrá que restar al valor del
--- presupuesto inicial (columna presupuesto) los gastos que se han generado
--- (columna gastos). Tenga en cuenta que en algunos casos pueden existir
--- valores negativos. Utilice un alias apropiado para la nueva columna columna
--- que está calculando.
+create procedure eje11()
+begin
+    select nombre as Nombre_Departamento,
+           case 
+               when (presupuesto - gastos) >= 0 then (presupuesto - gastos)
+               else 0
+           end as Presupuesto_Disponible,
+           case 
+               when (presupuesto - gastos) < 0 then (presupuesto - gastos)
+               else 0
+           end as Presupuesto_Negativo
+    from departamento;
+end //
+delimiter ;
+call eje11();
 
-select nombre AS Nombre_Departamento,
-case 
-    when (presupuesto - gastos) >= 0 then (presupuesto - gastos)
-    else 0
-end as Presupuesto_Disponible,
-case 
-    when (presupuesto - gastos) < 0 then (presupuesto - gastos)
-    else 0
-end as Presupuesto_Negativo
-from departamento;
+	-- 12.Lista el nombre de los departamentos y el valor del presupuesto actual
+	-- ordenado de forma ascendente.
+    delimiter //
+    create procedure eje12()
+    begin
+    	select nombre, presupuesto - gastos as presupuesto_actual
+		from departamento
+		order by presupuesto_actual asc;
+    end //
+    delimiter ;
+    call eje12();
 
+	-- 13.Lista el nombre de todos los departamentos ordenados de forma
+	-- ascendente.
+    delimiter //
+    create procedure eje13()
+    begin
+    	select nombre
+		from departamento
+		order by nombre asc;
+    end //
+    delimiter ;
+    call eje13();
 
--- 12. Lista el nombre de los departamentos y el valor del presupuesto actual
--- ordenado de forma ascendente.
+	-- 14.Lista el nombre de todos los departamentos ordenados de forma
+	-- descendente.
+    delimiter //
+    create procedure eje14()
+    begin
+    	select nombre
+		from departamento
+		order by nombre desc;
+    end //
+    delimiter ;
+call eje14();
 
-select nombre, presupuesto
-from departamento
-order by presupuesto asc;
+	-- 15.Lista los apellidos y el nombre de todos los empleados, ordenados de forma
+	-- alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su
+	-- nombre.
+    delimiter //
+    create procedure eje15()
+    begin
+    	select apellido1 as apellido, apellido2 as segundo_apellido , nombre 
+		from empleado
+		order by apellido1, apellido2, nombre;
+    end //
+    delimiter ;
+    call eje15();
 
-
--- 13. Lista el nombre de todos los departamentos ordenados de forma
--- ascendente.
-
-select nombre
-from departamento
-order by nombre asc;
-
-
--- 14. Lista el nombre de todos los departamentos ordenados de forma
--- descendente.
-
-select nombre
-from departamento
-order by nombre desc;	
-
--- 15.Lista los apellidos y el nombre de todos los empleados, ordenados de forma
--- alfabética tendiendo en cuenta en primer lugar sus apellidos y luego su
--- nombre.
-
-select apellido1, apellido2, nombre
-from empleado
-order by apellido1 asc, apellido2 asc, nombre asc;
-
--- 16. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos
--- que tienen mayor presupuesto.
-
-select nombre, presupuesto
-from departamento
-order by presupuesto desc
-limit 3;
-
--- 17. Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos
--- que tienen menor presupuesto.
-
-select nombre, presupuesto
-from departamento
-order by presupuesto asc
-limit 3;
-
--- 18. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que
--- tienen mayor gasto.
-
-select nombre, gastos
-from departamento
-order by gastos desc
-limit 2;
-
--- 19. Devuelve una lista con el nombre y el gasto, de los 2 departamentos que
--- tienen menor gasto.
-
-select nombre, gastos
-from departamento
-order by gastos asc
-limit 2;
-
--- 20. Devuelve una lista con 5 filas a partir de la tercera fila de la tabla empleado. La
--- tercera fila se debe incluir en la respuesta. La respuesta debe incluir todas las
--- columnas de la tabla empleado.
-
-select *
-from empleado
-limit 2, 5;
-
--- 21. Devuelve una lista con el nombre de los departamentos y el presupuesto, de
--- aquellos que tienen un presupuesto mayor o igual a 150000 euros.
-
-select nombre, presupuesto
-from departamento
-where presupuesto >= 150000;
-
--- 22. Devuelve una lista con el nombre de los departamentos y el gasto, de
--- aquellos que tienen menos de 5000 euros de gastos.
-
-select nombre, gastos
-from departamento
-where gastos < 5000;
-
--- 23. Devuelve una lista con el nombre de los departamentos y el presupuesto, de
--- aquellos que tienen un presupuesto entre 100000 y 200000 euros. Sin
--- utilizar el operador BETWEEN.
-
-select nombre, presupuesto
-from departamento
-where presupuesto >= 100000 and presupuesto <= 200000;
-
--- 24. Devuelve una lista con el nombre de los departamentos que no tienen un
--- presupuesto entre 100000 y 200000 euros. Sin utilizar el operador BETWEEN.
-
-select nombre
-from departamento
-where presupuesto < 100000 or presupuesto > 200000;
-
--- 25. Devuelve una lista con el nombre de los departamentos que tienen un
--- presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
-
-select nombre
-from departamento
-where presupuesto between 100000 and 200000;
-
--- 26. Devuelve una lista con el nombre de los departamentos que no tienen un
--- presupuesto entre 100000 y 200000 euros. Utilizando el operador BETWEEN.
-
-select nombre
-from departamento
-where not (presupuesto between 100000 and 200000);
-
-
--- 27. Devuelve una lista con el nombre de los departamentos, gastos y
--- presupuesto, de aquellos departamentos donde los gastos sean mayores
--- que el presupuesto del que disponen.
-
-select nombre, gastos, presupuesto
-from departamento
-where gastos > presupuesto;
-
-
--- 28. Devuelve una lista con el nombre de los departamentos, gastos y
--- presupuesto, de aquellos departamentos donde los gastos sean menores
--- que el presupuesto del que disponen.
-
-select nombre, gastos, presupuesto
-from departamento
-where gastos < presupuesto;
-
-
--- 29. Devuelve una lista con el nombre de los departamentos, gastos y
--- presupuesto, de aquellos departamentos donde los gastos sean iguales al
--- presupuesto del que disponen.
-
-select *
-from empleado
-where apellido2 is null;
-
-
--- 30. Lista todos los datos de los empleados cuyo segundo apellido sea NULL.
-
-select *
-from empleado
-where apellido2 is null;
+	-- 16.Devuelve una lista con el nombre y el presupuesto, de los 3 departamentos
+	-- que tienen mayor presupuesto.
+    delimiter //
+    create procedure eje16()
+    begin
+    	select nombre, presupuesto 
+		from departamento 
+		order by presupuesto desc
+		limit 3;
+    end //
+    delimiter ;
+    call eje16();
+    
 
